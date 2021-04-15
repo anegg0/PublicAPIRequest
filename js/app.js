0ccb322292b74df5a8dd33f3b289448478d4d9d5
Gael Blanchemain <anegg0@pop-os.localdomain>
2021-04-15 15:06:12 -0700

    Add generateHTML

--- js/app.js
+++ js/app.js
@@ -1,4 +1,67 @@
-fetch('https://randomuser.me/api/')
+const gallery = document.getElementById('gallery');
+
+function getEmployees(json){
+    let employee;
+employee = json.results.map(employee => {
+console.log(employee.name.first);
+console.log(employee.name.last);
+console.log(employee.email);
+console.log(employee.picture.thumbnail);
+
+});
+}
+
+function generateHTML(data){
+    // console.log(data.results);
+data.results.map(employee => {
+
+const card = document.createElement('div');
+card.setAttribute('class','card');
+gallery.appendChild(card);
+card.innerHTML = `
+                <div class="card">
+                    <div class="card-img-container">
+                        <img class="card-img" src="${employee.picture.thumbnail}" alt="profile picture">
+                    </div>
+                    <div class="card-info-container">
+                        <h3 id="name" class="card-name cap">${employee.name.first} ${employee.name.last}</h3>
+                        <p class="card-text">${employee.email}</p>
+                        <p class="card-text cap">${employee.location.city}, ${employee.location.state}</p>
+                    </div>
+                </div>
+`;
+// const imageContainer = document.createElement('div');
+// imageContainer.setAttribute('class', 'card-img-container');
+// card.appendChild(imageContainer);
+// let image = document.createElement('img');
+// image.setAttribute('src', employee.picture.thumbnail);
+// image.setAttribute('alt', 'profile picture');
+// imageContainer.appendChild(image);
+// const infoContainer = document.createElement('div');
+// infoContainer.setAttribute('class', 'card-info-container');
+// card.appendChild(infoContainer);
+// const name = document.createElement('h3');
+// name.setAttribute('class', 'card-name cap');
+// name.setAttribute('id', 'name');
+// infoContainer.appendChild(name);
+
+// card.innerHTML = `
+// <h2>${data}</h2>
+// `;
+});
+}
+
+window.addEventListener("load", (e) => { 
+    fetch('https://randomuser.me/api/?results=12')
 .then(response => response.json())
-.then(data => console.log(data));
-.catch( err => console.log('Error Fetching API: ', err)
+// .then(getEmployees)
+.then(generateHTML)
+// .then(data => console.log(data))
+// .then(data => console.log(data.results[0].email))
+// .then(data => console.log(data.results[0]))
+// .then(data => console.log(data.results[0].name.first))
+.catch(err => console.log('Error Fetching API: ', err));
+});
+
+
+
