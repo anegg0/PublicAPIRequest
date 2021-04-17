c4ae1173f19a4787bb855ef6611f08e6f712ca3e
Gael Blanchemain <anegg0@pop-os.localdomain>
2021-04-16 18:12:35 -0700

    Add Modal

--- js/app.js
+++ js/app.js
@@ -1,21 +1,20 @@
 const gallery = document.getElementById('gallery');
 
-function getEmployees(json){
-    let employee;
-employee = json.results.map(employee => {
-console.log(employee.name.first);
-console.log(employee.name.last);
-console.log(employee.email);
-console.log(employee.picture.thumbnail);
+// function getEmployees(json){
+//     let employee;
+// employee = json.results.map(employee => {
+// console.log(employee.name.first);
+// console.log(employee.name.last);
+// console.log(employee.email);
+// console.log(employee.picture.thumbnail);
 
-});
-}
+// });
+// }
 
 function generateHTML(data){
 data.results.map(employee => {
 
 const card = document.createElement('div');
-card.setAttribute('class','card');
 gallery.appendChild(card);
 card.innerHTML = `
                 <div class="card">
@@ -30,10 +29,11 @@ card.innerHTML = `
                 </div>
 `;
 });
+generateModal();
 return data;
 }
 
-function generateModal(data){
+function generateModal(){
 // modalContainer.setAttribute('class','modal-container');
   const modalConstantData = `
             <div class="modal-container">
@@ -43,33 +43,50 @@ function generateModal(data){
                     </div>
                 </div>`;
     var div = document.createElement('div');
+    div.setAttribute('class', 'wrapper');
     div.innerHTML = modalConstantData;
-                gallery.insertAdjacentElement('afterEnd', div );
+                gallery.insertAdjacentElement('afterend', div );
 div.style.display = 'none';
 // gallery.appendChild(modalContainer);
 const modalCloseButton = div.querySelector('.modal-close-btn');
-    console.log(modalCloseButton);
 modalCloseButton.addEventListener('click', (e) => {
 div.style.display = 'none';
 });
 }
 
 function updateModal(employee) {
+// data.results.map(employee => {
+    // console.log(`for updateModal, employee is: ${employee.results}`);
 const modalInfoContainer = document.querySelector('.modal-info-container');
 modalInfoContainer.innerHTML = '';
-var modalCustomContent = `<img class="modal-img" src="https://placehold.it/125x125" alt="profile picture">
-                        <h3 id="name" class="modal-name cap">name</h3>
-                        <p class="modal-text">email</p>
-                        <p class="modal-text cap">city</p>
+var modalCustomContent = `<img class="modal-img" src="${employee.picture.medium}" alt="profile picture">
+                        <h3 id="name" class="modal-name cap">${employee.name.first} ${employee.name.last}</h3>
+                        <p class="modal-text">${employee.email}</p>
+                        <p class="modal-text cap">${employee.location.city}</p>
                         <hr>
-                        <p class="modal-text">(555) 555-5555</p>
-                        <p class="modal-text">123 Portland Ave., Portland, OR 97204</p>
-                        <p class="modal-text">Birthday: 10/21/2015</p>`;
+                        <p class="modal-text">${employee.cell}</p>
+                        <p class="modal-text">${employee.location.street.number} ${employee.location.street.name}, ${employee.location.city}, ${employee.location.state} ${employee.location.postcode}</p>
+                        <p class="modal-text">Birthday: ${(employee.dob.date).slice(0,10)}</p>`;
                         
 modalInfoContainer.insertAdjacentHTML("afterbegin",modalCustomContent );
 
+// });
 }
 
+function addClickHandlers(data) {
+  const cardArray = document.getElementsByClassName('card');
+//   console.log(cardArray);
+    //  console.log(data);
+  for (let i = 0; i < cardArray.length; i++) {
+    //   console.log(data);
+ cardArray[i].addEventListener('click', (event)  => {
+     const wrapper = document.querySelector('.wrapper');
+     wrapper.style.display = "flex";
+     console.log(data.results[i]);
+    updateModal(data.results[i]);
+});
+  }
+}
 
 
 window.addEventListener("load", (e) => { 
@@ -77,7 +94,9 @@ window.addEventListener("load", (e) => {
 .then(response => response.json())
 // .then(getEmployees)
 .then(generateHTML)
-.then(generateModal)
+// .then(generateModal)
+// .then(updateModal)
+.then(addClickHandlers)
 // .then(data => console.log(data))
 // .then(data => console.log(data.results[0].email))
 // .then(data => console.log(data.results[0]))
